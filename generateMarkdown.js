0ae46754018e6d059792e1d689efc731f705687c
Jo Hill <jos.a.hill@gmail.com>
2022-02-15 13:50:33 -0800

    updated naming convention for code

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,15 +1,15 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-const licenseArr = ['Apache', 'GPL v2', 'GPL v3', 'MIT']
+const licenses = ['Apache', 'GPL v2', 'GPL v3', 'MIT']
 
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-    if (license === licenseArr[0]) {
+    if (license === licenses[0]) {
         return "[![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
-    } else if (license === licenseArr[1]) {
+    } else if (license === licenses[1]) {
         return "[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)"
-    } else if (license === licenseArr[2]) {
+    } else if (license === licenses[2]) {
         return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
-    } else if (license === licenseArr[3]) {
+    } else if (license === licenses[3]) {
         return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
     } else {
         return ""
@@ -19,14 +19,14 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-    if (license === licenseArr[0]) {
-        return `[${licenseArr[0]}](https://opensource.org/licenses/Apache-2.0)`
-    } else if (license === licenseArr[1]) {
-        return `[${licenseArr[1]}](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`
-    } else if (license === licenseArr[2]) {
-        return `[${licenseArr[2]}](https://www.gnu.org/licenses/gpl-3.0)`
-    } else if (license === licenseArr[3]) {
-        return `[${licenseArr[3]}](https://opensource.org/licenses/MIT)`
+    if (license === licenses[0]) {
+        return `[${licenses[0]}](https://opensource.org/licenses/Apache-2.0)`
+    } else if (license === licenses[1]) {
+        return `[${licenses[1]}](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`
+    } else if (license === licenses[2]) {
+        return `[${licenses[2]}](https://www.gnu.org/licenses/gpl-3.0)`
+    } else if (license === licenses[3]) {
+        return `[${licenses[3]}](https://opensource.org/licenses/MIT)`
     } else {
         return ""
     }
@@ -35,14 +35,14 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-    if (license === licenseArr[0]) {
-        return `Get more information about ${licenseArr[0]} here:`
-    } else if (license === licenseArr[1]) {
-        return `Get more information about ${licenseArr[1]} here:`
-    } else if (license === licenseArr[2]) {
-        return `Get more information about ${licenseArr[2]} here:`
-    } else if (license === licenseArr[3]) {
-        return `Get more information about ${licenseArr[3]} here:`
+    if (license === licenses[0]) {
+        return `Get more information about ${licenses[0]} here:`
+    } else if (license === licenses[1]) {
+        return `Get more information about ${licenses[1]} here:`
+    } else if (license === licenses[2]) {
+        return `Get more information about ${licenses[2]} here:`
+    } else if (license === licenses[3]) {
+        return `Get more information about ${licenses[3]} here:`
     } else {
         return ""
     }
